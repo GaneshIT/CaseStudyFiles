7ecf337d2ca53009ac3f19fe1af31fe8d8af01a7
GaneshIT <ganesh.ceoit@gmail.com>
2025-04-02 16:38:50 +0530

    updated

---
+++ ConsoleApp4/BridgeDesign.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    // This is going to be an interface that acts as a bridge between the abstraction classes and implementer classes
+    // The Implementor Interface defines the operations for all implementation classes.
+    // It doesn't have to match the Abstraction's interface. 
+    // In fact, the two interfaces can be entirely different. 
+
+    public interface ILEDTV
+    {
+        void SwitchOn();
+        void SwitchOff();
+        void SetChannel(int channelNumber);
+    }
+
+    // This is going to be a class which implements the ILEDTV interface and 
+    // also provide the implementation details for the associated Abstraction class.
+    // Each Concrete Implementation corresponds to a specific platform
+
+    public class SamsungLedTv : ILEDTV
+    {
+        public void SwitchOn()
+        {
+            Console.WriteLine("Turning ON : Samsung TV");
+        }
+        public void SwitchOff()
+        {
+            Console.WriteLine("Turning OFF : Samsung TV");
+        }
+        public void SetChannel(int channelNumber)
+        {
+            Console.WriteLine("Setting channel Number " + channelNumber + " on Samsung TV");
+        }
+    }
+
+    // Each Concrete Implementation corresponds to a specific platform
+    // This is going to be a class and should implement the Implementation interface
+    public class SonyLedTv : ILEDTV
+    {
+        public void SwitchOn()
+        {
+            Console.WriteLine("Turning ON : Sony TV");
+        }
+        public void SwitchOff()
+        {
+            Console.WriteLine("Turning OFF : Sony TV");
+        }
+        public void SetChannel(int channelNumber)
+        {
+            Console.WriteLine("Setting channel Number " + channelNumber + " on Sony TV");
+        }
+    }
+
+    //This is an abstract class that contains members that define an abstract business object and its functionality.
+    //It contains a reference to an object of type ILEDTV and delegates all of the real work to this object.
+    //It can also act as the base class for other abstractions.
+
+    public abstract class AbstractRemoteControl
+    {
+        protected ILEDTV ledTv;
+        public abstract void SwitchOn();
+        public abstract void SwitchOff();
+        public abstract void SetChannel(int channelNumber);
+    }
+
+    // This is going to be a concrete class which inherits from the Abstraction class i.e. AbstractRemoteControl. 
+    // This Redefined Abstraction Class extends the interface defined by AbstractRemoteControl class.
+    public class SamsungRemoteControl : AbstractRemoteControl
+    {
+        public SamsungRemoteControl(ILEDTV ledTv)
+        {
+            this.ledTv = ledTv;
+        }
+        public override void SwitchOn()
+        {
+            ledTv.SwitchOn();
+        }
+        public override void SwitchOff()
+        {
+            ledTv.SwitchOff();
+        }
+        public override void SetChannel(int channelNumber)
+        {
+            ledTv.SetChannel(channelNumber);
+        }
+    }
+
+    // This is going to be a concrete class which inherits from the Abstraction class i.e. AbstractRemoteControl. 
+    // This Redefined Abstraction Class extends the interface defined by AbstractRemoteControl class.
+    public class SonyRemoteControl : AbstractRemoteControl
+    {
+        public SonyRemoteControl(ILEDTV ledTv)
+        {
+            this.ledTv = ledTv;
+        }
+        public override void SwitchOn()
+        {
+            ledTv.SwitchOn();
+        }
+        public override void SwitchOff()
+        {
+            ledTv.SwitchOff();
+        }
+        public override void SetChannel(int channelNumber)
+        {
+            ledTv.SetChannel(channelNumber);
+        }
+    }
+
+    // This is going to be a concrete class which inherits from the Abstraction class i.e. AbstractRemoteControl. 
+    // This Redefined Abstraction Class extends the interface defined by AbstractRemoteControl class.
+    public class LGRemoteControl : AbstractRemoteControl
+    {
+        public LGRemoteControl(ILEDTV ledTv)
+        {
+            this.ledTv = ledTv;
+        }
+        public override void SwitchOn()
+        {
+            ledTv.SwitchOn();
+        }
+        public override void SwitchOff()
+        {
+            ledTv.SwitchOff();
+        }
+        public override void SetChannel(int channelNumber)
+        {
+            ledTv.SetChannel(channelNumber);
+        }
+    }
+}
--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -33,72 +33,111 @@ Console.WriteLine(Class1.maxSum(arr, n, k));
 //Documents documents=new Documents(_wordReport);
 //documents.Airtel();
 
-
+#region prototype
 // Creating an Instance of Permanent Employee Class
-Employee emp1 = new PermanentEmployee()
+//Employee emp1 = new PermanentEmployee()
+//{
+//    Name = "Anurag",
+//    Department = "IT",
+//    Type = "Permanent",
+//    Salary = 100000
+//};
+
+////Creating a Clone of the above Permanent Employee
+//Employee emp2 = emp1.GetClone();
+
+//// Changing the Name and Department Property Value of emp2 instance, 
+//// will not change the Name and Department Property Value of the emp1 instance
+//emp2.Name = "Pranaya";
+//emp2.Department = "HR";
+//emp1.ShowDetails();
+//emp2.ShowDetails();
+
+//emp1.Name = "Raj";
+//emp1.ShowDetails();
+//emp2.ShowDetails();
+//// Creating an Instance of Temporary Employee Class
+//Employee emp3 = new TemporaryEmployee()
+//{
+//    Name = "Preety",
+//    Department = "HR",
+//    Type = "Temporary",
+//    FixedAmount = 200000
+//};
+
+////Creating a Clone of the above Temporary Employee
+//Employee emp4 = emp3.GetClone();
+
+//// Changing the Name and Department Property Value of emp4 instance, 
+//// will not change the Name and Department Property Value of the emp3 instance
+//emp4.Name = "Priyanka";
+//emp4.Department = "Sales";
+
+
+//emp3.ShowDetails();
+//emp4.ShowDetails();
+
+//Console.Read();
+
+#endregion
+
+#region builder
+//// Constructing the PDF Report
+//// Step1: Create a Builder Object 
+//// Creating PDFReport Builder Object
+//PDFReport pdfReport = new PDFReport();
+
+//// Step2: Pass the Builder Object to the Director
+//// First Create an instance of ReportDirector
+//ReportDirector reportDirector = new ReportDirector();
+//// Then Pass the pdfReport Builder Object to the MakeReport Method of ReportDirector
+//// The ReportDirector will return one of the Representations of the Product
+//Report report = reportDirector.MakeReport(pdfReport);
+
+//// Step3: Display the Report by calling the DisplayReport method of the Product
+//report.DisplayReport();
+
+//Console.WriteLine("-------------------");
+//// Constructing the Excel Report
+//// The Process is going to be the same
+//ExcelReport excelReport = new ExcelReport();
+//report = reportDirector.MakeReport(excelReport);
+//report.DisplayReport();
+#endregion
+
+#region adapter
+//Storing the Employees Data in a String Array
+string[,] employeesArray = new string[5, 4]
 {
-    Name = "Anurag",
-    Department = "IT",
-    Type = "Permanent",
-    Salary = 100000
+                {"101","John","SE","10000"},
+                {"102","Smith","SE","20000"},
+                {"103","Dev","SSE","30000"},
+                {"104","Pam","SE","40000"},
+                {"105","Sara","SSE","50000"}
 };
+//The EmployeeAdapter Makes it possible to work with Two Incompatible Interfaces
+Console.WriteLine("HR system passes employee string array to Adapter\n");
+ITarget target = new EmployeeAdapter();
+target.ProcessCompanySalary(employeesArray);
+#endregion
 
-//Creating a Clone of the above Permanent Employee
-Employee emp2 = emp1.GetClone();
-
-// Changing the Name and Department Property Value of emp2 instance, 
-// will not change the Name and Department Property Value of the emp1 instance
-emp2.Name = "Pranaya";
-emp2.Department = "HR";
-emp1.ShowDetails();
-emp2.ShowDetails();
-
-emp1.Name = "Raj";
-emp1.ShowDetails();
-emp2.ShowDetails();
-// Creating an Instance of Temporary Employee Class
-Employee emp3 = new TemporaryEmployee()
-{
-    Name = "Preety",
-    Department = "HR",
-    Type = "Temporary",
-    FixedAmount = 200000
-};
-
-//Creating a Clone of the above Temporary Employee
-Employee emp4 = emp3.GetClone();
-
-// Changing the Name and Department Property Value of emp4 instance, 
-// will not change the Name and Department Property Value of the emp3 instance
-emp4.Name = "Priyanka";
-emp4.Department = "Sales";
+#region bridge
+// Except for the initialization phase, where an Abstraction object gets i.e. SonyRemoteControl or SamsungRemoteControl
+// linked with a specific Implementation object i.e. new SonyLedTv() or new SamsungLedTv(), 
+// the client code should only depend on the Abstraction class i.e. SonyRemoteControl or SamsungRemoteControl. 
+AbstractRemoteControl sonyRemoteControl = new SonyRemoteControl(new SonyLedTv());
+sonyRemoteControl.SwitchOn();
+sonyRemoteControl.SetChannel(101);
+sonyRemoteControl.SwitchOff();
+Console.WriteLine();
+AbstractRemoteControl samsungRemoteControl = new SamsungRemoteControl(new SamsungLedTv());
+samsungRemoteControl.SwitchOn();
+samsungRemoteControl.SetChannel(202);
+samsungRemoteControl.SwitchOff();
+#endregion
 
 
-emp3.ShowDetails();
-emp4.ShowDetails();
+int a = 10;
+object b = a;
 
-Console.Read();
-
-
-
-// Constructing the PDF Report
-// Step1: Create a Builder Object 
-// Creating PDFReport Builder Object
-PDFReport pdfReport = new PDFReport();
-
-// Step2: Pass the Builder Object to the Director
-// First Create an instance of ReportDirector
-ReportDirector reportDirector = new ReportDirector();
-// Then Pass the pdfReport Builder Object to the MakeReport Method of ReportDirector
-// The ReportDirector will return one of the Representations of the Product
-Report report = reportDirector.MakeReport(pdfReport);
-
-// Step3: Display the Report by calling the DisplayReport method of the Product
-report.DisplayReport();
-
-Console.WriteLine("-------------------");
-// Constructing the Excel Report
-// The Process is going to be the same
-ExcelReport excelReport = new ExcelReport();
-report = reportDirector.MakeReport(excelReport);
-report.DisplayReport();
+string c = b;
